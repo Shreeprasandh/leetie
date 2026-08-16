b6bc2fd4f7a12eaa442e385014e9cc4b30deb586
Shreeprasandh <shreeprasandh@users.noreply.github.com>
2026-08-16 21:16:15 +0530

    leetie: Add 2029. Stone Game IX [Medium] (java)

---
+++ solutions/Medium/2029-stone-game-ix/solution.java
@@ -0,0 +1,42 @@
+// ──────────────────────────────────────────────────
+// Problem  : 2029. Stone Game IX
+// Difficulty: Medium
+// Tags     : N/A
+// Link     : https://leetcode.com/problems/stone-game-ix/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42508000 (beats 0%)
+// Language : java
+// Copyright: (c) 2026 Shreeprasandh K. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int c0 = 0;
+        int c1 = 0;
+        int c2 = 0;
+
+        // Count stones according to their remainder
+        for (int stone : stones) {
+
+            if (stone % 3 == 0) {
+                c0++;
+            } 
+            else if (stone % 3 == 1) {
+                c1++;
+            } 
+            else {
+                c2++;
+            }
+        }
+
+        // Case 1: Number of remainder-0 stones is even
+        if (c0 % 2 == 0) {
+            return c1 > 0 && c2 > 0;
+        }
+
+        // Case 2: Number of remainder-0 stones is odd
+        return Math.abs(c1 - c2) > 2;
+    }
+}
