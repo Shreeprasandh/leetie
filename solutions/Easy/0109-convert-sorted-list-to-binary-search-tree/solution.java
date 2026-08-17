67df47c99ba1a656dfbec035cd473cb556f98db0
Shreeprasandh <shreeprasandh@users.noreply.github.com>
2026-08-17 23:54:39 +0530

    leetie: Add 109. Convert Sorted List to Binary Search TreeMed. [Easy] (java)

---
+++ solutions/Easy/0109-convert-sorted-list-to-binary-search-tree/solution.java
@@ -0,0 +1,32 @@
+// ──────────────────────────────────────────────────
+// Problem  : 109. Convert Sorted List to Binary Search TreeMed.
+// Difficulty: Easy
+// Tags     : N/A
+// Link     : https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42708000 (beats 0%)
+// Language : java
+// Copyright: (c) 2026 Shreeprasandh K. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head==null)
+            return null;
+        if(head.next==null)
+            return new TreeNode(head.val);
+        ListNode slow=head;
+        ListNode fast=head.next.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        TreeNode res=new TreeNode(slow.next.val);
+        ListNode righthalf=slow.next.next;
+        slow.next=null;
+        res.left=sortedListToBST(head);
+        res.right=sortedListToBST(righthalf);
+        return res;
+    }
+}
