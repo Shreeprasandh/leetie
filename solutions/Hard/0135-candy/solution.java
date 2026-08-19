ef43a23f70a1878432b85b162980661f5471865c
Shreeprasandh <shreeprasandh@users.noreply.github.com>
2026-08-19 11:36:05 +0000

    leetie: Add 135. Candy [Hard] (java)

---
+++ solutions/Hard/0135-candy/solution.java
@@ -0,0 +1,29 @@
+// ──────────────────────────────────────────────────
+// Problem  : 135. Candy
+// Difficulty: Hard
+// Tags     : Array, Greedy
+// Link     : https://leetcode.com/problems/candy/
+// Runtime  : 3 ms (beats 86%)
+// Memory   : 51804000 (beats 7%)
+// Language : java
+// Copyright: (c) 2026 Shreeprasandh. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int cnt = 0;
+        int[] candies = new int[n];
+        for (int i = 0; i < n; i++) candies[i] = 1;
+        for (int i = 1; i < n; i++)
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        for (int i = n - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                candies[i - 1] = Math.max(candies[i] + 1, candies[i - 1]);
+            cnt += candies[i - 1];
+        }
+        return cnt + candies[n - 1];
+    }
+}
