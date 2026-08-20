4e175b8676270cd00ef3da86566cd6299c8a9796
Shreeprasandh <shreeprasandh@users.noreply.github.com>
2026-08-20 04:03:01 +0000

    leetie: Add 289. Game of Life [Medium] (java)

---
+++ solutions/Medium/0289-game-of-life/solution.java
@@ -0,0 +1,85 @@
+// ──────────────────────────────────────────────────
+// Problem  : 289. Game of Life
+// Difficulty: Medium
+// Tags     : Array, Matrix, Simulation
+// Link     : https://leetcode.com/problems/game-of-life/
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 43524000 (beats 11%)
+// Language : java
+// Copyright: (c) 2026 Shreeprasandh. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int r = board.length;
+        int c = board[0].length;
+        int[][] currBoard = new int[r][c];
+        
+        
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                currBoard[i][j] = board[i][j];
+        
+        
+        for (int i = 0; i < r; i++) {
+            int nxtRow = i + 1;
+            int prevRow = i - 1;
+            
+            
+            boolean isNxtRowInRange = nxtRow < r;
+            boolean isPrevRowInRange = prevRow >= 0;
+            
+            
+            for (int j = 0; j < c; j++) {
+                int liveNeighbors = 0;
+                int nxtCol = j + 1;
+                int prevCol = j - 1;
+                
+                
+                boolean isNxtColInRange = nxtCol < c;
+                boolean isPrevColInRange = prevCol >= 0;
+                
+                
+                if (isNxtRowInRange) {
+                    liveNeighbors += currBoard[nxtRow][j];
+                    
+                    
+                    if (isPrevColInRange)
+                        liveNeighbors += currBoard[nxtRow][prevCol];
+                    
+                    
+                    if (isNxtColInRange)
+                        liveNeighbors += currBoard[nxtRow][nxtCol];
+                }
+                
+                
+                if (isPrevRowInRange) {
+                    liveNeighbors += currBoard[prevRow][j];
+                    
+                    
+                    if (isPrevColInRange)
+                        liveNeighbors += currBoard[prevRow][prevCol];
+                    
+                    
+                    if (isNxtColInRange)
+                        liveNeighbors += currBoard[prevRow][nxtCol];
+                }
+                
+                
+                if (isPrevColInRange)
+                    liveNeighbors += currBoard[i][prevCol];
+                
+                
+                if (isNxtColInRange)
+                    liveNeighbors += currBoard[i][nxtCol];
+                
+                
+                if (currBoard[i][j] == 1)
+                    board[i][j] = liveNeighbors == 2 || liveNeighbors == 3 ? 1 : 0;
+                else
+                    board[i][j] = liveNeighbors == 3 ? 1 : 0;
+            }
+        }
+    }
+}
