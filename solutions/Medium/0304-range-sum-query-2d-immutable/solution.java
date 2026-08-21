dc1f7e455b3a9b539aafdbf89336d8392789e63d
Shreeprasandh <shreeprasandh@users.noreply.github.com>
2026-08-21 04:06:51 +0000

    leetie: Add 304. Range Sum Query 2D - Immutable [Medium] (java)

---
+++ solutions/Medium/0304-range-sum-query-2d-immutable/solution.java
@@ -0,0 +1,31 @@
+// ──────────────────────────────────────────────────
+// Problem  : 304. Range Sum Query 2D - Immutable
+// Difficulty: Medium
+// Tags     : Array, Design, Matrix, Prefix Sum
+// Link     : https://leetcode.com/problems/range-sum-query-2d-immutable/
+// Runtime  : 100 ms (beats 86%)
+// Memory   : 141912000 (beats 57%)
+// Language : java
+// Copyright: (c) 2026 Shreeprasandh. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class NumMatrix {
+    private int[][] sums;
+    
+    public NumMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        sums = new int[n+1][m+1];
+        for (int i=1; i<=n; i++)
+            for (int j=1; j<=m; j++)
+                sums[i][j] = sums[i-1][j] + sums[i][j-1] + matrix[i-1][j-1] - sums[i-1][j-1];
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int RED_RECTANGLE = sums[row2+1][col2+1];
+        int PURPLE_RECTANGLES = sums[row1][col2+1] + sums[row2+1][col1];
+        int BLUE_RECTANGLE = sums[row1][col1];
+        return RED_RECTANGLE - PURPLE_RECTANGLES + BLUE_RECTANGLE;
+    }
+}
