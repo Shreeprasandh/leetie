58ddbc637bb4b651ae1a469bccf0508f9d1fd042
Shreeprasandh <shreeprasandh@users.noreply.github.com>
2026-08-21 03:42:31 +0000

    leetie: Add 3116. Kth Smallest Amount With Single Denomination Combination [Hard] (java)

---
+++ solutions/Hard/3116-kth-smallest-amount-with-single-denomination-combination/solution.java
@@ -0,0 +1,60 @@
+// ──────────────────────────────────────────────────
+// Problem  : 3116. Kth Smallest Amount With Single Denomination Combination
+// Difficulty: Hard
+// Tags     : Array, Math, Binary Search, Bit Manipulation, Combinatorics, Number Theory
+// Link     : https://leetcode.com/problems/kth-smallest-amount-with-single-denomination-combination/
+// Runtime  : 219 ms (beats 6%)
+// Memory   : 43256000 (beats 96%)
+// Language : java
+// Copyright: (c) 2026 Shreeprasandh. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    private int[] coins;
+    private int k;
+
+    public long findKthSmallest(int[] coins, int k) {
+        this.coins = coins;
+        this.k = k;
+        long l = 1, r = (long) 1e11;
+        while(l < r) {
+            long mid = (l+r) >> 1;
+            if(check(mid)) {
+                r = mid;
+            } else {
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+
+    private boolean check(long mx) {
+        long cnt = 0;
+        int n = coins.length;
+        for(int i = 1; i < 1 << n; ++i) {
+            long v = 1;
+            for(int j = 0; j < n; ++j) {
+                if((i >> j & 1) == 1) {
+                    v = lcm(v, coins[j]);
+                    if(v > mx) break;
+                }
+            }
+            int m = Integer.bitCount(i);
+            if(m % 2 == 1) {
+                cnt += mx / v;
+            } else {
+                cnt -= mx / v;
+            }
+        }
+        return cnt >= k;
+    }
+
+    private long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    private long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a%b);
+    }
+}
