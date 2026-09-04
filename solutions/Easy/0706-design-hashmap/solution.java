25b87e2baa9c343357bda9cbe9cc17a79b3b647b
Shreeprasandh <shreeprasandh@users.noreply.github.com>
2026-09-04 07:30:12 +0000

    leetie: Add 706. Design HashMap [Easy] (java)

--- solutions/Easy/0706-design-hashmap/solution.java
+++ solutions/Easy/0706-design-hashmap/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, Linked List, Design, Hash Function
 // Link     : https://leetcode.com/problems/design-hashmap/
-// Runtime  : 2 ms (beats 0%)
-// Memory   : 45792000 (beats 0%)
+// Runtime  : 33 ms (beats 42%)
+// Memory   : 57392000 (beats 53%)
 // Language : java
 // Copyright: (c) 2026 Shreeprasandh. All rights reserved.
 // Synced by: leetie
