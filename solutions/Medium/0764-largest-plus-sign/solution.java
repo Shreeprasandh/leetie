ec8e0184d769c3b0dba512bc622af4334a46f5d7
Shreeprasandh <shreeprasandh@users.noreply.github.com>
2026-09-06 02:15:16 +0000

    leetie: Add 764. Largest Plus Sign [Medium] (java)

--- solutions/Medium/0764-largest-plus-sign/solution.java
+++ solutions/Medium/0764-largest-plus-sign/solution.java
@@ -3,105 +3,65 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming
 // Link     : https://leetcode.com/problems/largest-plus-sign/
-// Runtime  : 49 ms (beats 37%)
-// Memory   : 142820000 (beats 7%)
+// Runtime  : 18 ms (beats 99%)
+// Memory   : 48204000 (beats 75%)
 // Language : java
 // Copyright: (c) 2026 Shreeprasandh. All rights reserved.
 // Synced by: leetie
 // ──────────────────────────────────────────────────
 
 class Solution {
-    
-    public class Total {
-        int l;
-        int r;
-        int u;
-        int d;
-        
-        Total(int l, int r, int u, int d){
-            this.l = l;
-            this.r = r;
-            this.u = u;
-            this.d = d;
+    public int orderOfLargestPlusSign(int n, int[][] mines) {
+        int dp[][] = new int[n][n];
+        for(int mine[] : mines) {
+            int x = mine[0], y = mine[1];
+            dp[x][y] = -1;
         }
-    }
-    
-    public int orderOfLargestPlusSign(int n, int[][] mine) {
-        int[][] mines = new int[n][n];
-        
-        
-        for (int[] row : mines) {
-            Arrays.fill(row, 1);
-        }
-        
-        
-        for (int[] m : mine) {
-            mines[m[0]][m[1]] = 0;
-        }
-        
-        Total[][] nums = new Total[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                nums[i][j] = new Total(0, 0, 0, 0);
-            }
-        }
-        
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = 0; j < n; j++) {
-                if (mines[i][j] == 0) {
-                    sum = 0;
-                    nums[i][j].l = sum;
-                } else {
-                    nums[i][j].l = ++sum;
+
+        for(int i=0; i<n; ++i) {
+            int prefix = 0;
+            for(int j=0; j<n; ++j) {
+                if(dp[i][j] == -1) {
+                    prefix = 0;
+                    continue;
                 }
+                ++prefix;
+                dp[i][j] = prefix;
             }
-        }
-        
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = n - 1; j >= 0; j--) {
-                if (mines[i][j] == 0) {
-                    sum = 0;
-                    nums[i][j].r = sum;
-                } else {
-                    nums[i][j].r = ++sum;
+            int suffix = 0;
+            for(int j=n-1; j>=0; --j) {
+                if(dp[i][j] == -1) {
+                    suffix = 0;
+                    continue;
                 }
+                ++suffix;
+                dp[i][j] = Math.min(dp[i][j], suffix);
             }
         }
-        
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = 0; j < n; j++) {
-                if (mines[j][i] == 0) {
-                    sum = 0;
-                    nums[j][i].u = sum;
-                } else {
-                    nums[j][i].u = ++sum;
+
+        int maxPlus = 0;
+        for(int j=0; j<n; ++j) {
+            int prefix = 0;
+            for(int i=0; i<n; ++i) {
+                if(dp[i][j] == -1) {
+                    prefix = 0;
+                    continue;
                 }
+                ++prefix;
+                dp[i][j] = Math.min(dp[i][j], prefix);
             }
-        }
-        
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = n - 1; j >= 0; j--) {
-                if (mines[j][i] == 0) {
-                    sum = 0;
-                    nums[j][i].d = sum;
-                } else {
-                    nums[j][i].d = ++sum;
+            int suffix = 0;
+            for(int i=n-1; i>=0; --i) {
+                if(dp[i][j] == -1) {
+                    suffix = 0;
+                    continue;
                 }
+                ++suffix;
+                dp[i][j] = Math.min(dp[i][j], suffix);
+                maxPlus = Math.max(maxPlus, dp[i][j]);
             }
         }
-        
-        int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums[0].length; j++) {
-                max = Math.max(Math.min(nums[i][j].l, Math.min(nums[i][j].r, Math.min(nums[i][j].d, nums[i][j].u))), max);
-            }
-        }
-        
-        return max;
+
+        return maxPlus;
     }
 }
